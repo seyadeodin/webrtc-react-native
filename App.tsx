b5defa865ee408854d7d153a86b1ff7ac743d4a1
lucsa <seyadeodin@gmail.com>
2022-06-14 20:38:11 -0300

    created base app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { mediaDevices, RTCView } from 'react-native-webrtc';
 
-export default function App() {
+function App() {
+  const [stream, setStream] = useState(null);
+  const start = async () => {
+    console.log('start');
+    if (!stream) {
+      let s;
+      try {
+        s = await mediaDevices.getUserMedia({ video: true });
+        setStream(s);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+  };
+  const stop = () => {
+    console.log('stop');
+    if (stream) {
+      stream.release();
+      setStream(null);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+      {
+        stream &&
+          <RTCView
+            streamURL={stream.toURL()}
+            style={styles.stream} />
+      }
+        <View
+          style={styles.footer}>
+          <Button
+            title = "Start"
+            onPress = {start} />
+          <Button
+            title = "Stop"
+            onPress = {stop} />
+        </View>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    backgroundColor: Colors.white,
+    ...StyleSheet.absoluteFill
+  },
+  stream: {
+    flex: 1
+  },
+  footer: {
+    backgroundColor: Colors.lighter,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
   },
 });
+
+export default App;
